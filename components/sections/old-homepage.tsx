51598937b660daa048a505bd16eeec3af623fdb1
Ritwik Mishra <dhdeveloper19@gmail.com>
2026-01-22 12:41:43 +0530

    blog css homepage

--- components/sections/old-homepage.tsx
+++ components/sections/old-homepage.tsx
@@ -923,7 +923,7 @@ export function OldHomepage() {
 
       {/* Blogs Carousel Section */}
       {blogs.length > 0 && (
-        <section className="py-16 bg-white">
+        <section className="py-16 bg-white homepage-blogs-sec">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="text-center mb-12">
               <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-4 text-uppercase">
